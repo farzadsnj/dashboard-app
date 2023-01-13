74c89d6c795cf1fbd54309feebf73878ced92119
farzadsnj <farzadsanjarani3@gmail.com>
2023-01-13 19:35:14 +0330

    co7

--- src/components/GeoChart.jsx
+++ src/components/GeoChart.jsx
@@ -6,141 +6,81 @@ import { useTheme } from '@mui/material'
 import { tokens } from '../theme'
 
 const GeoChart = ({isDashboard = false}) => {
-    const theme = useTheme()
-    const colors = tokens(theme.palette.mode)
-
-  return (
-    <ResponsiveChoropleth
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    return (
+      <ResponsiveChoropleth
         data={data}
         theme={{
-            axis:{
-                domain:{
-                    line:{
-                        stroke: colors.gray[100]
-                    },
-                },
-                legend:{
-                    text:{
-                        fill: colors.gray[100]
-                    }
-                },
-                ticks:{
-                    line:{
-                        stroke: colors.gray[100],
-                        strokeWidth: 1
-                    },
-                    text:{
-                        fill:colors.gray[100]
-                    }
-                }
+          axis: {
+            domain: {
+              line: {
+                stroke: colors.gray[100],
+              },
             },
-            legends:{
-                text:{
-                    fill:colors.gray[100]
-                }
+            legend: {
+              text: {
+                fill: colors.gray[100],
+              },
             },
-            tooltip:{
-                container:{
-                    color: colors.primary[500]
-                }
-            }
-          }}
+            ticks: {
+              line: {
+                stroke: colors.gray[100],
+                strokeWidth: 1,
+              },
+              text: {
+                fill: colors.gray[100],
+              },
+            },
+          },
+          legends: {
+            text: {
+              fill: colors.gray[100],
+            },
+          },
+        }}
         features={geoFeatures.features}
         margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
-        // colors="nivo"
-        domain={[ 0, 1000000 ]}
+        domain={[0, 1000000]}
         unknownColor="#666666"
         label="properties.name"
         valueFormat=".2s"
-        projectionType="naturalEarth1"
         projectionScale={isDashboard ? 40 : 150}
-        projectionTranslation={isDashboard ? [0.49,0.6] : [ 0.5, 0.5 ]}
-        projectionRotation={[ 0, 0, 0 ]}
-        enableGraticule={true}
-        // graticuleLineColor="#d6d6d6"
+        projectionTranslation={isDashboard ? [0.49, 0.6] : [0.5, 0.5]}
+        projectionRotation={[0, 0, 0]}
         borderWidth={1.5}
         borderColor="#ffffff"
-        // defs={[
-        //     {
-        //         id: 'dots',
-        //         type: 'patternDots',
-        //         background: 'inherit',
-        //         color: '#38bcb2',
-        //         size: 4,
-        //         padding: 1,
-        //         stagger: true
-        //     },
-        //     {
-        //         id: 'lines',
-        //         type: 'patternLines',
-        //         background: 'inherit',
-        //         color: '#eed312',
-        //         rotation: -45,
-        //         lineWidth: 6,
-        //         spacing: 10
-        //     },
-        //     {
-        //         id: 'gradient',
-        //         type: 'linearGradient',
-        //         colors: [
-        //             {
-        //                 offset: 0,
-        //                 color: '#000'
-        //             },
-        //             {
-        //                 offset: 100,
-        //                 color: 'inherit'
-        //             }
-        //         ]
-        //     }
-        // ]}
-        // fill={[
-        //     {
-        //         match: {
-        //             id: 'CAN'
-        //         },
-        //         id: 'dots'
-        //     },
-        //     {
-        //         match: {
-        //             id: 'CHN'
-        //         },
-        //         id: 'lines'
-        //     },
-        //     {
-        //         match: {
-        //             id: 'ATA'
-        //         },
-        //         id: 'gradient'
-        //     }
-        // ]}
-        legends={ !isDashboard ?
-            [{
-                anchor: 'bottom-left',
-                direction: 'column',
-                justify: true,
-                translateX: 20,
-                translateY: -100,
-                itemsSpacing: 0,
-                itemWidth: 94,
-                itemHeight: 18,
-                itemDirection: 'left-to-right',
-                itemTextColor: '#fff',
-                itemOpacity: 0.85,
-                symbolSize: 18,
-                effects: [
+        legends={
+          !isDashboard
+            ? [
+                {
+                  anchor: "bottom-left",
+                  direction: "column",
+                  justify: true,
+                  translateX: 20,
+                  translateY: -100,
+                  itemsSpacing: 0,
+                  itemWidth: 94,
+                  itemHeight: 18,
+                  itemDirection: "left-to-right",
+                  itemTextColor: colors.gray[100],
+                  itemOpacity: 0.85,
+                  symbolSize: 18,
+                  effects: [
                     {
-                        on: 'hover',
-                        style: {
-                            itemTextColor: '#fff',
-                            itemOpacity: 1
-                        }
-                    }
-                ]
-            }
-        ] : undefined}
-    />
-  )
+                      on: "hover",
+                      style: {
+                        itemTextColor: "#ffffff",
+                        itemOpacity: 1,
+                      },
+                    },
+                  ],
+                },
+              ]
+            : undefined
+        }
+      />
+    );
 }
 
 export default GeoChart
--- src/components/PieChart.jsx
+++ src/components/PieChart.jsx
@@ -4,55 +4,56 @@ import { mockPieData as data } from "../data/MockData";
 import { tokens } from "../theme";
 import { useTheme } from "@mui/material";
 
-const PieChart = ({isDashboard = false}) => {
-    const theme = useTheme()
-    const colors = tokens(theme.palette.mode)
+const PieChart = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
   return (
     <ResponsivePie
       data={data}
       theme={{
-        axis:{
-            domain:{
-                line:{
-                    stroke: colors.gray[100]
-                },
+        axis: {
+          domain: {
+            line: {
+              stroke: colors.gray[100],
             },
-            legend:{
-                text:{
-                    fill: colors.gray[100]
-                }
+          },
+          legend: {
+            text: {
+              fill: colors.gray[100],
             },
-            ticks:{
-                line:{
-                    stroke: colors.gray[100],
-                    strokeWidth: 1
-                },
-                text:{
-                    fill:colors.gray[100]
-                }
-            }
+          },
+          ticks: {
+            line: {
+              stroke: colors.gray[100],
+              strokeWidth: 1,
+            },
+            text: {
+              fill: colors.gray[100],
+            },
+          },
         },
-        legends:{
-            text:{
-                fill:colors.gray[100]
-            }
-        }
+        legends: {
+          text: {
+            fill: colors.gray[100],
+          },
+        },
       }}
       margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
       innerRadius={0.5}
       padAngle={0.7}
       cornerRadius={3}
       activeOuterRadiusOffset={8}
-      borderWidth={1}
       borderColor={{
         from: "color",
-        modifiers: [["brighter", "0.3"]],
+        modifiers: [["darker", 0.2]],
       }}
       arcLinkLabelsSkipAngle={10}
-      arcLinkLabelsTextColor="#fff"
+      arcLinkLabelsTextColor={colors.gray[100]}
       arcLinkLabelsThickness={2}
       arcLinkLabelsColor={{ from: "color" }}
-      arcLabelsSkipAngle={10}
+      enableArcLabels={false}
+      arcLabelsRadiusOffset={0.4}
+      arcLabelsSkipAngle={7}
       arcLabelsTextColor={{
         from: "color",
         modifiers: [["darker", 2]],
@@ -75,56 +76,6 @@ const PieChart = ({isDashboard = false}) => {
           rotation: -45,
           lineWidth: 6,
           spacing: 10,
-        },
-      ]}
-      fill={[
-        {
-          match: {
-            id: "ruby",
-          },
-          id: "dots",
-        },
-        {
-          match: {
-            id: "c",
-          },
-          id: "dots",
-        },
-        {
-          match: {
-            id: "go",
-          },
-          id: "dots",
-        },
-        {
-          match: {
-            id: "python",
-          },
-          id: "dots",
-        },
-        {
-          match: {
-            id: "scala",
-          },
-          id: "lines",
-        },
-        {
-          match: {
-            id: "lisp",
-          },
-          id: "lines",
-        },
-        {
-          match: {
-            id: "elixir",
-          },
-          id: "lines",
-        },
-        {
-          match: {
-            id: "javascript",
-          },
-          id: "lines",
         },
       ]}
       legends={[
